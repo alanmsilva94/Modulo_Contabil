39372637ed3f6232efdd39046da3437441072e28
Alan Moura <156733008+alanmsilva94@users.noreply.github.com>
2024-10-01 23:30:10 -0300

    Atualizar o ModuloContabil.java

--- ModucoContabil.java
+++ ModucoContabil.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class ModucoContabil {
+public class ModuloContabil {
 
     public static void main(String[] args) {
 
